9e54e594f3f729e2a42d5a6886c62b34d6ee7f55
bika ridjalluddin <bikarjx@gmail.com>
2025-06-17 01:09:24 +0700

    Expanded project dependencies by adding `UMG` module to `XFrame.Build.cs`.

--- Source/XFrame/XFrame.Build.cs
+++ Source/XFrame/XFrame.Build.cs
@@ -17,7 +17,8 @@ public class XFrame : ModuleRules
 			"EnhancedInput",
 			"GameplayAbilities",
 			"GameplayTasks",
-			"GameplayTags"
+			"GameplayTags",
+			"UMG"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
