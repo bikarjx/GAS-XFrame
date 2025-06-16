4825f92969b626973018246cd6cd89b256b36b27
bika ridjalluddin <bikarjx@gmail.com>
2025-06-17 00:14:06 +0700

    Added replication logic to `UXFAttributeSet` with health and mana attributes, implemented replication notification handlers, and updated header file with necessary properties and methods.

--- Source/XFrame/Private/AbilitySystem/XFAttributeSet.cpp
+++ Source/XFrame/Private/AbilitySystem/XFAttributeSet.cpp
@@ -2,4 +2,41 @@
 
 
 #include "AbilitySystem/XFAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+
+UXFAttributeSet::UXFAttributeSet()
+{
+	
+}
+
+void UXFAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UXFAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UXFAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UXFAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UXFAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UXFAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UXFAttributeSet, Health, OldHealth);
+}
+
+void UXFAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UXFAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UXFAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UXFAttributeSet, Mana, OldMana);
+}
+
+void UXFAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UXFAttributeSet, MaxMana, OldMaxMana);
+}
