1a95d7879cf8b64e4a1b74d7b779a54d24e48725
bika ridjalluddin <bikarjx@gmail.com>
2025-06-17 12:44:32 +0700

    "Added `AXFHUD` class, removed unused include from `XFWidgetController`, and imported new widget assets (`WBP_GlobeProgressBar`, `WBP_HealthGlobe`, `WBP_ManaGlobe`, `WBP_Overlay`)."

---
+++ Source/XFrame/Public/UI/HUD/XFHUD.h
@@ -0,0 +1,17 @@
+// Copyrights, Bikarjx 2025. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "XFHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XFRAME_API AXFHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
--- Source/XFrame/Public/UI/WidgetController/XFWidgetController.h
+++ Source/XFrame/Public/UI/WidgetController/XFWidgetController.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
 #include "XFWidgetController.generated.h"
 
 class UAttributeSet;
